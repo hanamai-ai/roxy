8b4487a7b84d0c84ec5b095774a30a7ffad8203e
ugol <ugo.landini@gmail.com>
2025-08-17 18:04:56 +0200

    minor fixes

--- v2/src/resp.c
+++ v2/src/resp.c
@@ -1,9 +1,12 @@
 #include "resp.h"
 #include <string.h>
 #include <limits.h>
+#include <ctype.h>
 
 static void str_to_upper(char *s) {
-    for (; *s; ++s) if (*s >= 'a' && *s <= 'z') *s = (char)(*s - 'a' + 'A');
+    for (; *s; s++) {
+        *s = (char) toupper((unsigned char)*s);
+    }
 }
 
 static long parse_crlf_number(const char *s, size_t n, size_t *digits_out) {
@@ -25,23 +28,25 @@ static long parse_crlf_number(const char *s, size_t n, size_t *digits_out) {
     return LONG_MIN;
 }
 
-int resp_parse_request(const char *buf, size_t len, size_t *frame_len, struct roxy_cmd *out) {
+int resp_parse_request(const char *buf, const size_t len, size_t *frame_len, struct roxy_cmd *out) {
     if (len==0)
         return 0;
-    size_t pos = 0;
     if (buf[0] == '*') {
-        size_t used; long nitems = parse_crlf_number(buf+1, len-1, &used);
-        if (nitems == LONG_MIN) return 0;
-        if (nitems <= 0 || nitems > ROXY_MAX_ARGS) return -1;
+        size_t pos = 0;
+        size_t used;
+        const long num_items = parse_crlf_number(buf+1, len-1, &used);
+        if (num_items == LONG_MIN) return 0;
+        if (num_items <= 0 || num_items > ROXY_MAX_ARGS) return -1;
         pos = 1 + used;
         out->argc = 0;
-        for (long i=0; i<nitems; ++i) {
+        for (long i=0; i<num_items; ++i) {
             if (pos >= len) return 0;
             if (buf[pos] != '$') return -1;
-            size_t u2; long blen = parse_crlf_number(buf+pos+1, len-pos-1, &u2);
+            size_t u2;
+            long blen = parse_crlf_number(buf+pos+1, len-pos-1, &u2);
             if (blen == LONG_MIN) return 0;
             if (blen < 0) return -1;
-            size_t hdr = 1 + u2;
+            const size_t hdr = 1 + u2;
             if (pos + hdr + (size_t)blen + 2 > len) return 0;
             const char *b = buf + pos + hdr;
             out->argv[out->argc].ptr = b;
@@ -49,8 +54,8 @@ int resp_parse_request(const char *buf, size_t len, size_t *frame_len, struct ro
             out->argc++;
             pos += hdr + (size_t)blen + 2;
         }
-        size_t n0 = out->argv[0].len;
-        size_t copy = n0 < sizeof(out->cmd)-1 ? n0 : sizeof(out->cmd)-1;
+        const size_t n0 = out->argv[0].len;
+        const size_t copy = n0 < sizeof(out->cmd)-1 ? n0 : sizeof(out->cmd)-1;
         memcpy(out->cmd, out->argv[0].ptr, copy);
         out->cmd[copy] = '\0';
         str_to_upper(out->cmd);
@@ -64,7 +69,7 @@ int resp_parse_request(const char *buf, size_t len, size_t *frame_len, struct ro
     out->argc = 0;
     size_t i=0; while (&buf[i] < cr && out->argc < ROXY_MAX_ARGS) {
         while (&buf[i] < cr && (buf[i]==' '||buf[i]=='\t')) i++;
-        size_t start = i;
+        const size_t start = i;
         while (&buf[i] < cr && buf[i]!=' ' && buf[i]!='\t') i++;
         if (i>start) {
             out->argv[out->argc].ptr = buf+start;
